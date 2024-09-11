c6cdc88ea0129eac53651b56d9a9c91f6e5d59bc
evelynvic23 <evelynvic2310@gmail.com>
2024-09-11 12:35:20 -0300

    Finalização do construtor

--- Aula-06/aula06-encapsulamento/src/App.java
+++ Aula-06/aula06-encapsulamento/src/App.java
@@ -1,13 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+
+
         // Criando a instância
 
-        Aluno evelyn = new Aluno();
-        evelyn.setNome("Evelyn");
-        System.out.println(evelyn.getNome());
+        Aluno evelyn = new Aluno("Evelyn Victoria", 2023, 18, "feminino", true);
+        
+        // evelyn.setNome("Evelyn");
 
-        Aluno victoria = new Aluno();
-        evelyn.setNome("Victoria");
+        // System.out.println(evelyn.getNome());
+
+        // Aluno victoria = new Aluno();
+        // evelyn.setNome("Victoria");
+
         System.out.println(evelyn.getNome());
 
 
