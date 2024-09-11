7876ee9516e5edad0f2951134b5a929440536347
evelynvic23 <evelynvic2310@gmail.com>
2024-09-11 12:13:53 -0300

    Encapsulamento

---
+++ Aula-06/aula06-encapsulamento/src/Aluno.java
@@ -0,0 +1,25 @@
+public class Aluno {
+
+    //Sintaxe de atributos
+
+    // metodo_de_acesso tipagem nome
+
+    private String nome;
+    private int ra;
+    private int idade;
+    private String genero;
+    private boolean status;
+
+    //
+
+    public void setNome (String ent_nome){
+        nome = ent_nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+
+    
+}
---
+++ Aula-06/aula06-encapsulamento/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Criando a instância
+        Aluno evelyn = new Aluno();
+
+        evelyn.setNome("Evelyn Victoria");
+
+
+    }
+}
