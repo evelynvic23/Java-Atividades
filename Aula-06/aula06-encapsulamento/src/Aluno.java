7b8f8645ead2fd534c81d3737e6a3087b00a5495
evelynvic23 <evelynvic2310@gmail.com>
2024-09-11 12:29:08 -0300

    Criação de Construtor

--- Aula-06/aula06-encapsulamento/src/Aluno.java
+++ Aula-06/aula06-encapsulamento/src/Aluno.java
@@ -10,7 +10,18 @@ public class Aluno {
     private String genero;
     private boolean status;
 
-    //
+    //Contrutor não tem retorno
+
+    //Criação de construtor Aluno
+    
+    public Aluno(String nome, int ra, int idade, String genero, boolean status){
+
+        this.nome = nome;
+        this.ra = ra;
+        this.idade = idade;
+        this.genero = genero;
+        this.status = status;
+    }
 
     public void setNome (String ent_nome){
         nome = ent_nome;
--- Aula-06/aula06-encapsulamento/src/App.java
+++ Aula-06/aula06-encapsulamento/src/App.java
@@ -1,9 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Criando a instância
-        Aluno evelyn = new Aluno();
 
-        evelyn.setNome("Evelyn Victoria");
+        Aluno evelyn = new Aluno();
+        evelyn.setNome("Evelyn");
+        System.out.println(evelyn.getNome());
+
+        Aluno victoria = new Aluno();
+        evelyn.setNome("Victoria");
+        System.out.println(evelyn.getNome());
 
 
     }
