81ae4efd5f0c166df3fcd4d59983ebe0958c71f8
evelynvic23 <115710253+evelynvic23@users.noreply.github.com>
2024-09-18 10:57:19 -0300

    String to float

--- Aula-06/aula06/src/App.java
+++ Aula-06/aula06/src/App.java
@@ -2,6 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Aula 07 Conversão");
 
+        String st, st2;
+        float real, real2;
+        int inteiro1, inteiro2;
+
+
+        //Convertendo String para float
+
+        st = "1.2";
+
+        real = (float) (new Float(st).floatValue());
+
+        System.out.println("String para float: " + real);
+
         
 
     }
